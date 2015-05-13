a4f72f7ade1466d3b0ea96557c853d4a32e9c0a8
danrubel <danrubel@google.com>
2015-05-12 20:45:12 -0700

    display env var during build

--- tool/grind.dart
+++ tool/grind.dart
@@ -16,13 +16,21 @@ void buildbot() => null;
 @Task('Gather and send coverage data.')
 void coverage() {
   final String coverageToken = Platform.environment['TRAVIS'];
-  print('coverageToken = $coverageToken');
+
+  Platform.environment.keys.toList()
+    ..sort()
+    ..forEach((String key) {
+      print('$key = ${Platform.environment[key]}');
+    });
 
   if (coverageToken != null) {
     PubApp coverallsApp = new PubApp.global('dart_coveralls');
-    coverallsApp.run(['report',
-      '--token', coverageToken,
-      '--retry', '2',
+    coverallsApp.run([
+      'report',
+      '--token',
+      coverageToken,
+      '--retry',
+      '2',
       '--exclude-test-files',
       'test/all.dart'
     ]);
