70a0d328f457a80891ecf4a794ed359cbcc4f437
danrubel <danrubel@google.com>
2015-05-12 19:41:48 -0700

    switch to grinder, run tests, add coverage

---
+++ test/all.dart
@@ -0,0 +1,9 @@
+library test.all;
+
+import 'ddns_updater_test.dart' as ddns_updater_test;
+import 'public_address_test.dart' as public_address_test;
+
+main() {
+  ddns_updater_test.main();
+  public_address_test.main();
+}
